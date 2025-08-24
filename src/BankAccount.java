cabcec9fe7dfb3e8b3b95cb158e3bbd712dd589f
sadidsiddique <sadidsiddiquev3@gmail.com>
2025-08-24 16:25:24 +0600

    created BankAccount.java

---
+++ src/BankAccount.java
@@ -0,0 +1,18 @@
+public class BankAccount {
+   
+    private String accountNumber;
+    private String accountHolderName;
+    private double balance;
+
+    public BankAccount(String accountNumber, String accountHolderName, double initialBalance) {
+        }
+    public void deposit(double amount) {
+
+           }
+    public void withdraw(double amount) {
+          }
+
+       public void checkBalance() {
+       
+    }
+}
